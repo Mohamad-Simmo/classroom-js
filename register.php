5ed90ad21ffb210a8060384d8d00d7afdba7be1a
mohammad semmo <mohammadsemmo81@gmail.com>
2022-05-15 00:48:37 +0300

    finalize form validation

--- register.php
+++ register.php
@@ -2,54 +2,126 @@
     $title = 'Register';
     include('partials/header.php');
 
-    $emailErrText = $emailErrClass = $emailAddr = '';
+    $optionsErrText = '';
+    $teacher = $student = false;
+    $fnameErrText = $fnameErrClass = $fnameVal = '';
+    $lnameErrText = $lnameErrClass = $lnameVal = '';
+    $emailErrText = $emailErrClass = $emailAddrVal = '';
+    $passErrText = $passErrClass = $passVal = '';
+    $passConfErrText = $passConfErrClass = $passConfVal = '';
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $error = false;
+
+      if (empty($_POST["options-outlined"])) {
+        $optionsErrText = "Please select your occupation.";
+      }
+      else {
+        if (test_input($_POST["options-outlined"]) == "teacher") {
+          $teacher = true;
+          $type = 0;
+        }
+        elseif (test_input($_POST["options-outlined"]) == "student") {
+          $student = true;
+          $type = 1;
+        }
+      }
+      
 
       if (empty($_POST["fname"])) {
-        echo "<script> setInvalid('#inputFname'); </script>";
+        $fnameErrText = "Please enter your first name.";
+        $fnameErrClass = "is-invalid";
+        $error = true;
       }
       else {
         $fname = test_input($_POST["fname"]);
         if (!preg_match("/^[a-z ,.'-]+$/i", $fname)) {
-          
+          $fnameErrClass = "is-invalid";
+          $fnameErrText = "Please enter a valid first name.";
+          $error = true;
+        }
+        else {
+          $fnameVal = $fname;
+          $fnameErrClass = 'is-valid';
         }
       }
   
       if (empty($_POST["lname"])) {
-        echo "<script> setInvalid('#inputLname'); </script>";
+        $lnameErrClass = "is-invalid";
+        $lnameErrText = "Please enter your last name.";
+        $error = true;
       }
       else {
         $lname = test_input($_POST["lname"]);
         if (!preg_match("/^[a-z ,.'-]+$/i", $lname)) {
-          
+          $lnameErrClass = "is-invalid";
+          $lnameErrText = "Please enter a valid last name.";
+          $error = true;
+        }
+        else {
+          $lnameErrClass = "is-valid";
+          $lnameVal = $lname;
         }
       }
   
-      $email = test_input($_POST["email"]);
       if (empty($_POST["email"])) {
         $emailErrClass = 'is-invalid';
         $emailErrText = 'Please enter an email address.';
+        $error = true;
       }
       else {
+        $email = test_input($_POST["email"]);
         if ((!filter_var($email, FILTER_VALIDATE_EMAIL))) {
           $emailErrClass = 'is-invalid';
           $emailErrText = 'Please enter a valid email address.';
+          $emailAddrVal = '';
+          $error = true;
         }
-        $emailErrClass = "is-valid";
-        $emailAddr = $_POST["email"];
+        //TODO: get emails and check if email exists.
+        else {
+          $emailErrClass = "is-valid";
+          $emailAddrVal = $email;
+        }
       }
-  
-      $password = test_input($_POST["password"]);
-      $confirmPassword = test_input($_POST["confpassword"]);
+      
+      if (empty($_POST["password"])) {
+        $passErrClass = "is-invalid";
+        $passErrText = "Please enter a password.";
+        $error = true;
+      }
+      else {
+        $password = test_input($_POST["password"]);
+        if (strlen($password) < 8) {
+          $passErrClass = "is-invalid";
+          $passErrText = "Password must be at least 8 characters.";
+          $error = true;
+        }
+        else {
+          $passErrClass = "is-valid";
+          $passVal = $password;
+        }
+      }
+
+      //$passConfErrText $passConfErrClass
+      if (empty($_POST["confpassword"])) {
+        $passConfErrClass = "is-invalid";
+        $passConfErrText = "Please confirm password.";
+        $error = true;
+      }
+      else {
+        $confPass = test_input($_POST["confpassword"]);
+        if ($confPass !== $password) {
+          $passConfErrClass = "is-invalid";
+          $passConfErrText = "Passwords must match.";
+          $error = true;
+        }
+        else {
+          $passConfErrClass = "is-valid";
+          $passConfVal = $confPass;
+        }
+      }
   
       
-      if (test_input($_POST["options-outlined"]) == "teacher") {
-        $type = 0;
-      }
-      elseif (test_input($_POST["options-outlined"]) == "student") {
-        $type = 1;
-      }
   
       /* $query = "INSERT INTO users VALUES ('DEFAULT','$fname', '$lname', '$email', '$password', '$type')";
       if (mysqli_query($conn, $query)) {
@@ -72,53 +144,77 @@
 
 <div id="div-register" style="height: 100vh;"
   class="col-lg-3 col-md-4 col-sm-6 col-8 text-center mx-auto d-flex flex-column justify-content-center">
-  <img src="images/book.png" class="img-fluid mx-auto mb-4 d-block" alt=""
+  <img src="images/book.png" class="img-fluid mx-auto mb-2 d-block" alt=""
     style="width: 75px;
     height: auto;">
-  <h1 class="fw-normal mb-3">Register</h1>
+  <h1 class="fw-normal mb-2">Register</h1>
 
   <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"
     method="post" class="text-center d-flex flex-column justify-content-center">
-    <div class="d-flex justify-content-center align-items-center mb-3 gap-4">
+
+    <div class="d-flex justify-content-center align-items-center mb-1 gap-4">
       <input type="radio" class="btn-check" name="options-outlined"
-        id="student-outlined" autocomplete="off" value="student">
+        id="student-outlined" autocomplete="off" value="student" <?php echo ($student ? 'checked':'');?>>
       <label class="btn btn-lg btn-outline-dark"
         for="student-outlined">Student</label>
         <div>Or</div>
       <input type="radio" class="btn-check" name="options-outlined"
-        id="teacher-outlined" value="teacher" autocomplete="off">
+        id="teacher-outlined" value="teacher" autocomplete="off" <?php echo ($teacher ? 'checked':'');?>>
       <label class="btn btn-lg btn-outline-dark"
         for="teacher-outlined">Teacher</label>
     </div>
-    <div class="d-flex gap-1">
-      <div class="form-floating mb-1">
-        <input type="text" class="form-control" id="inputFname"
-          placeholder="First name" name="fname">
-        <label for="inputFname">First name</label>
-      </div>
-      <div class="form-floating mb-1">
-        <input type="text" class="form-control" id="inputLname"
-          placeholder="Last name" name="lname">
-        <label for="inputLname">Last name</label>
-      </div>
+
+    <div class="options-error text-danger">
+      <?php echo $optionsErrText; ?>
     </div>
+    <div class="d-flex gap-1">
+
+      <div class="form-floating mb-1">
+        <input type="text" class="form-control <?php echo $fnameErrClass;?>" id="inputFname"
+          placeholder="First name" name="fname" value=<?php echo $fnameVal;?>>
+        <label for="inputFname">First name</label>
+        <div class="invalid-feedback text-start ms-2">
+        <?php echo $fnameErrText; ?>
+        </div>
+      </div>
+
+      <div class="form-floating mb-1">
+        <input type="text" class="form-control <?php echo $lnameErrClass; ?>" id="inputLname"
+          placeholder="Last name" name="lname" value=<?php echo $lnameVal; ?>>
+        <label for="inputLname">Last name</label>
+        <div class="invalid-feedback text-start ms-2">
+        <?php echo $lnameErrText; ?>
+        </div>
+      </div>
+
+    </div>
+
     <div class="form-floating mb-1">
       <input type="email" class="form-control <?php echo $emailErrClass;?>"
-        id="inputEmail" placeholder="Email" name="email" value="<?php echo $emailAddr;?>">
+        id="inputEmail" placeholder="Email" name="email" value="<?php echo $emailAddrVal;?>">
       <label for="inputEmail">Email address</label>
       <div class="invalid-feedback text-start ms-2">
         <?php echo $emailErrText; ?>
       </div>
     </div>
+
     <div class="form-floating mb-1">
-      <input type="password" class="form-control" id="inputPassword"
-        placeholder="Password" name="password">
+      <input type="password" class="form-control <?php echo $passErrClass; ?>" id="inputPassword"
+        placeholder="Password" name="password" value=<?php echo $passVal; ?>>
       <label for="inputPassword">Password</label>
+      <div class="invalid-feedback text-start ms-2">
+        <?php echo $passErrText; ?>
+      </div>
     </div>
+    
+
     <div class="form-floating mb-1">
-      <input type="password" class="form-control" id="inputConfPassword"
-        placeholder="Password" name="confpassword">
+      <input type="password" class="form-control <?php echo $passConfErrClass; ?>" id="inputConfPassword"
+        placeholder="Password" name="confpassword" value=<?php echo $passConfVal; ?>>
       <label for="inputPassword">Confirm Password</label>
+      <div class="invalid-feedback text-start ms-2"> 
+        <?php echo $passConfErrText; ?>
+      </div>
     </div>
 
     <button type="submit" class="btn btn-primary btn-lg">Register</button>

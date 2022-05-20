461c8bb2b8ad27fd8fd4092a6e777c60c08cee99
mohammad semmo <mohammadsemmo81@gmail.com>
2022-05-20 17:06:29 +0300

    add student page

---
+++ student.php
@@ -0,0 +1,20 @@
+<?php echo "student" ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <a href="logout.php" class="nav-link text-white d-flex align-items-center">
+    <i class="bi bi-box-arrow-left me-2 fs-4"></i>
+    <span class="d-none d-md-inline-block">Logout</span>
+  </a>
+</body>
+
+</html>

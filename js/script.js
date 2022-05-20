51b83405f92e92f6068762114a955ac5285ea6dd
mohammad semmo <mohammadsemmo81@gmail.com>
2022-05-20 17:09:15 +0300

    Add modal for new class

--- js/script.js
+++ js/script.js
@@ -18,29 +18,58 @@ document.querySelectorAll(".nav-link").forEach((link) => {
   });
 });
 
-//Create new class
-document.getElementById("confirm-class").addEventListener("click", () => {
-  let className = document.getElementById("class-name").value;
-  let classDescription = document.getElementById("class-description").value;
-  let classStudents = document.getElementById("class-students").value;
-  console.log(className + " " + classDescription + " " + classStudents);
+//bind modal elements
+let className = document.getElementById("class-name");
+let classDescription = document.getElementById("class-description");
+let classStudents = document.getElementById("class-students");
+let modalEl = document.getElementById("staticBackdrop");
+
+//when modal is dismissed clear input fields
+modalEl.addEventListener("hide.bs.modal", () => {
+  className.classList.remove("is-invalid");
+  className.value = "";
+  classDescription.value = "";
+  classStudents.value = "";
 });
 
+//Create new class
+document.getElementById("confirm-class").addEventListener("click", () => {
+  let modal = bootstrap.Modal.getInstance(modalEl);
+
+  if (!className.value.trim()) {
+    className.classList.add("is-invalid");
+  } else {
+    //Remove white space
+    classStudentsVal = classStudents.value;
+    classStudentsVal = classStudentsVal.trim();
+    classStudentsVal = classStudentsVal.replaceAll(" ", "");
+    classStudentsVal = classStudentsVal.replaceAll("\n", "");
+
+    //Create request
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        //TODO: show response
+        console.log(this.responseText);
+      }
+    };
+    xmlhttp.open("POST", "newClass.php", true);
+    xmlhttp.setRequestHeader(
+      "Content-type",
+      "application/x-www-form-urlencoded"
+    );
+    xmlhttp.send(
+      `className=${className.value}&classDescription=${classDescription.value}&classStudents=${classStudentsVal}`
+    );
+    //confirm clicked --> close modal
+    modal.hide();
+  }
+});
+
+//Bootstrap tooltips
 const tooltipTriggerList = document.querySelectorAll(
   '[data-bs-toggle="tooltip"]'
 );
 const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
-
-/* document.getElementById("new-class-btn").addEventListener("click", () => {
-  console.log("clicked");
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      console.log(this.responseText);
-    }
-  };
-  xmlhttp.open("POST", "testAjax.php");
-  xmlhttp.send();
-}); */

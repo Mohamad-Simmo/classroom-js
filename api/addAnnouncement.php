72d166cb417df9e1e9022fa3071e0410637f4114
mohammad semmo <mohammadsemmo81@gmail.com>
2022-05-28 19:27:36 +0300

    Create insert query

--- api/addAnnouncement.php
+++ api/addAnnouncement.php
@@ -2,10 +2,23 @@
   require '../partials/connection.php';
   session_start();
 
+  //TODO: error checking
   $userId = $_SESSION["id"];
-  $announcementTitle = $_POST["announcementTitle"];
-  $announcementBody = $_POST["announcementBody"];
+  $announcementTitle = trim($_POST["announcementTitle"]);
+  $announcementBody = trim($_POST["announcementBody"]);
+  $classCode = $_POST["classCode"];
 
+  $query = "INSERT INTO announcements
+            VALUES(
+              DEFAULT,
+              '$userId',
+              (SELECT id FROM classes WHERE code='$classCode'),
+              '$announcementTitle',
+              '$announcementBody',
+              DEFAULT)";
 
+   echo mysqli_query($conn, $query);
+
+  
   
 ?>

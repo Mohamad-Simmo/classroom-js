74705578f38c0e922584b1995c0cab901b218351
mohammad semmo <mohammadsemmo81@gmail.com>
2022-05-28 19:33:11 +0300

    include teacher/ student js

--- partials/footer.php
+++ partials/footer.php
@@ -3,8 +3,25 @@
   integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
   crossorigin="anonymous"></script>
 
+<?php 
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+  echo "<script>let type =".$_SESSION["type"]."</script>"; 
+}
+?>
+
+
+
 <script src="js/single_page.js"></script>
 <script src="js/script.js"></script>
+<?php
+if ($_SESSION["type"] == 0) {
+  echo '<script src="js/teacher.js"></script>';
+}
+else {
+  echo '<script src="js/student.js"></script>';
+}
+ 
+?>
 
 </body>
 

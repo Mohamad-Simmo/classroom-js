9d44d339cd167c0625c22eaf63a400678fb4db66
mohammad semmo <mohammadsemmo81@gmail.com>
2022-08-25 16:27:15 +0300

    refactor code

--- js/single_page.js
+++ js/single_page.js
@@ -1,42 +1,39 @@
 window.addEventListener('DOMContentLoaded', () => {
   //default load classes
-  loadClassesPage();
+  loadPage(sidebarClasses, containerClasses);
   console.log(type);
 });
 
-//Single page functions
-sidebarClasses = document.getElementById('sidebar-btn-classes');
-sidebarLorem = document.getElementById('sidebar-btn-lorem');
-sidebarIpsum = document.getElementById('sidebar-btn-ipsum');
-sidebarClasses.addEventListener('click', loadClassesPage);
-sidebarLorem.addEventListener('click', loadLoremPage);
-sidebarIpsum.addEventListener('click', loadIpsumPage);
+const sidebarClasses = document.getElementById('sidebar-btn-classes');
+const sidebarLorem = document.getElementById('sidebar-btn-lorem');
+const sidebarIpsum = document.getElementById('sidebar-btn-ipsum');
+const containerClasses = document.getElementById('classes-page-container');
+const containerLorem = document.getElementById('lorem-container');
+const containerIpsum = document.getElementById('ipsum-container');
+const containers = [containerClasses, containerLorem, containerIpsum];
+const navs = [sidebarClasses, sidebarLorem, sidebarIpsum];
 
-function loadClassesPage() {
-  document.getElementById('classes-page-container').style.display = 'block';
-  document.getElementById('lorem-container').style.display = 'none';
-  document.getElementById('ipsum-container').style.display = 'none';
-  sidebarClasses.classList.add('active');
-  sidebarLorem.classList.remove('active');
-  sidebarIpsum.classList.remove('active');
-  document.getElementById('new-class-btn').classList.remove('d-none');
-  document.getElementById('content-title').classList.remove('d-none');
+function loadPage(nav, container) {
+  containers.forEach((container) => {
+    container.style.display = 'none';
+  });
+  navs.forEach((nav) => {
+    nav.classList.remove('active');
+  });
 
-  loadClasses();
+  nav.classList.add('active');
+  container.style.display = 'block';
+  if (container === containerClasses) {
+    loadClasses();
+  }
 }
-function loadLoremPage() {
-  document.getElementById('classes-page-container').style.display = 'none';
-  document.getElementById('lorem-container').style.display = 'block';
-  document.getElementById('ipsum-container').style.display = 'none';
-  sidebarClasses.classList.remove('active');
-  sidebarLorem.classList.add('active');
-  sidebarIpsum.classList.remove('active');
-}
-function loadIpsumPage() {
-  document.getElementById('classes-page-container').style.display = 'none';
-  document.getElementById('lorem-container').style.display = 'none';
-  document.getElementById('ipsum-container').style.display = 'block';
-  sidebarClasses.classList.remove('active');
-  sidebarLorem.classList.remove('active');
-  sidebarIpsum.classList.add('active');
-}
+
+sidebarClasses.addEventListener('click', () => {
+  loadPage(sidebarClasses, containerClasses);
+});
+sidebarLorem.addEventListener('click', () =>
+  loadPage(sidebarLorem, containerLorem)
+);
+sidebarIpsum.addEventListener('click', () => {
+  loadPage(sidebarIpsum, containerIpsum);
+});

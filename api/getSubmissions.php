63bc7589c1c0505bdf44b3ae88b6f62462e60bb8
mohammad semmo <mohammadsemmo81@gmail.com>
2022-09-02 00:38:31 +0300

    Get submission count

--- api/getSubmissions.php
+++ api/getSubmissions.php
@@ -3,17 +3,30 @@
   session_start();
   
   $formId = $_GET["id"];
-  $query = "SELECT users.fname, users.lname, users.email, form_submissions.date_time, form_submissions.grade
-            FROM users 
-            JOIN form_submissions 
+
+  if (isset($_GET["count"])) {
+    $query = "SELECT COUNT(*) as c 
+              FROM form_submissions 
+              WHERE form_id=$formId";
+    $result = mysqli_query($conn, $query);
+    $response = mysqli_fetch_object($result)->c;
+    echo $response;
+  }
+  else {
+    $query = "SELECT users.fname, users.lname, users.email,
+             form_submissions.date_time, form_submissions.grade
+            FROM users
+            JOIN form_submissions
               ON form_submissions.user_id = users.id
             WHERE form_submissions.form_id = $formId";
 
-  $result = mysqli_query($conn, $query);
-  $response = [];
-  while ($row = mysqli_fetch_assoc($result)) {
-    $response[] = $row;
-  }
+    $result = mysqli_query($conn, $query);
+    $response = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+      $response[] = $row;
+    }
 
-  echo json_encode($response);
+    echo json_encode($response);
+  }
+  
 ?>

165f86fd0ee906ae4c676000c24fe911a8083ed9
mohammad semmo <mohammadsemmo81@gmail.com>
2022-09-02 00:38:15 +0300

    Compare timezone

--- api/getForms.php
+++ api/getForms.php
@@ -3,8 +3,20 @@
   session_start();
   
   $userId = $_SESSION["id"];
+
+  $query = "SELECT type FROM users WHERE id = $userId";
+  $result = mysqli_fetch_object(mysqli_query($conn, $query))->type;
+
+  $userType = $result;
+
+  date_default_timezone_set('Asia/Beirut');
+  $now = new DateTime();
+  $now = $now->format('Y-m-d H:i:s');
+
+
+
   $type = $_GET["type"];
-  //get forms for user joined classes
+  //get forms for user joined classes (not submitted and before due date)
   $query = "SELECT $type.form_id as id, forms.title, forms.start_date_time,
               forms.end_date_time, classes.name as class_name
             FROM $type
@@ -16,16 +28,19 @@
               SELECT form_submissions.user_id
               FROM form_submissions
               WHERE user_id = $userId AND form_submissions.form_id = $type.form_id
-            )";
+            )".($userType == 1 ? " AND forms.end_date_time > '$now'":"");
+
+
 
   $result = mysqli_query($conn, $query);
-
+  
   $response = [];
   while ($row = mysqli_fetch_assoc($result)) {
     $response[] = $row;
   }
+
   mysqli_free_result($result);
-  mysqli_close($conn);
+  mysqli_close($conn); 
   
-  echo json_encode($response);
+  echo json_encode($response); 
 ?>
